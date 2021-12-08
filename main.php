75348ea85fc51881464d813d7e25c3007a0bc5aa
河原瑞季 <kawaharamizuki@kawaharamizukinoMacBook-Air.local>
2021-12-08 22:04:40 +0900

    function化

--- main.php
+++ main.php
@@ -1,28 +1,39 @@
 <?php
-echo "先攻か後攻を選択してください！\n";
-echo "1:先攻,2:後攻\n";
-$player = trim(fgets(STDIN));
 
-if ($player = 1) {
-    $cpu = "2";
+function setTurn (){
+    echo "先攻か後攻を選択してください！\n";
+    echo "1:先攻,2:後攻\n";
+    $player = trim(fgets(STDIN));
+    
+    if ($player = 1) {
+        $cpu = "2";
+    }
+    else {
+        $cpu = 1;
+    }
+    return [$player, $cpu];
 }
-else {
-    $cpu = 1;
+
+function setPiece () {
+    echo "○がいいですか？×がいいですか？/n";
+    echo "1:○、2:×/n";
+    $playerPiece = trim(fgets(STDIN));
+
+    $selectInput = trim(fgets(STDIN));
+
+    if ($selectInput = 1) {
+        $playerPiece = 'o';
+        $cpuPiece    = 'x';
+    }
+    else {
+        $playerPiece = 'x';
+        $cpuPiece    = 'o';
+    }
+    return [$playerPiece, $cpuPiece];
 }
 
-// echo $cpu;
-// echo $player;
-
-echo "○がいいですか？×がいいですか？/n";
-echo "1:○、2:×/n";
-$playerPiece = trim(fgets(STDIN));
-
-
-if ($playerPiece = 1) {
-    $cpuPiece = 2;
-}
-else {
-    $cpu = 1;
-}
+// 関数を使う
+$setTurnArray = setTurn ();
+$setPieceArray = setPiece();
 
 echo "場所を指定してください！";
