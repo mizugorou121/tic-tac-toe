1720caabaa2548139883e10431a2760bbc07a802
katsugdgd <61843020+kachan0627@users.noreply.github.com>
2021-12-23 23:53:21 +0900

    function setTurn ()の$cpuをあらかじめ定義しておくことでelse文は書かなくて済みます。

--- main.php
+++ main.php
@@ -1,15 +1,13 @@
 <?php
 
 function setTurn (): array{
+    $cpu = 1;
     echo "先攻か後攻を選択してください！\n";
     echo "1:先攻,2:後攻\n";
     $player = trim(fgets(STDIN));
     
     if ($player == 1) {
         $cpu = 2;
-    }
-    else {
-        $cpu = 1;
     }
     return [$player, $cpu];
 }
